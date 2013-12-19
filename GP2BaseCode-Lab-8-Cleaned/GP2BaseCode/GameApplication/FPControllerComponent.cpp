6b1f5dfec7a3d27b8ec206687c3acae80bc36ef6
Alix1723 <alix1723@live.co.uk>
2013-12-19 23:36:04 +0000

    Simple boundary checking added

--- GP2BaseCode-Lab-8-Cleaned/GP2BaseCode/GameApplication/FPControllerComponent.cpp
+++ GP2BaseCode-Lab-8-Cleaned/GP2BaseCode/GameApplication/FPControllerComponent.cpp
@@ -34,29 +34,32 @@ void FPControllerComponent::update()
 			m_AimAngle.y -= delta.x*m_MouseSensitivity;
 		}
 
-		if(m_AimAngle.y>89.0f){m_AimAngle.y=89.0f;}		//Clamp the pitch between -90 and 90 degrees
+		if(m_AimAngle.y>89.0f){m_AimAngle.y=89.0f;}		//Clamp the pitch between -89 and 89 degrees
 		if(m_AimAngle.y<-89.0f){m_AimAngle.y=-89.0f;}
-			float d2r = (XM_PI * 2) / 360;	//Convert Degrees to Radians
+
+		float d2r = (XM_PI * 2) / 360;	//Convert Degrees to Radians
+
 		m_AimDirection.x = cos(m_AimAngle.x * d2r) * cos(m_AimAngle.y * d2r);		//Convert degrees into vector components via trigonomentry
-		m_AimDirection.z = sin(m_AimAngle.x * d2r) * cos(m_AimAngle.y * d2r);;			//Note: for this vector, XZ is horizontal rotation, Y is vertical rotation
+		m_AimDirection.z = sin(m_AimAngle.x * d2r) * cos(m_AimAngle.y * d2r);;		//Note: for this vector, XZ is horizontal rotation, Y is vertical rotation
 		m_AimDirection.y = sin(m_AimAngle.y * d2r);	
+
 		m_VecAimDirection = XMLoadFloat3(&m_AimDirection);
 		
 			//POSITION
 		if(m_ControllerInput)
 		{
-			iForward =  GetLeftThumbstick(0).y;
-			iHorizontal = GetLeftThumbstick(0).x;
+			iForward =  GetLeftThumbstick(0).y;			//Joystick inputs
+			iHorizontal = GetLeftThumbstick(0).x;		//Given as a value from -1 to 1 on each axis
 			iUpwards = GetRightTrigger(0) - GetLeftTrigger(0);
 			m_SpeedMultiplier = m_SpeedMultiplier + 0.00001f*(GetButtonPressed(0,XINPUT_GAMEPAD_RIGHT_SHOULDER)-GetButtonPressed(0,XINPUT_GAMEPAD_LEFT_SHOULDER));	//Modify how fast the player moves
-				if(m_SpeedMultiplier<=0){m_SpeedMultiplier=0.0001f;}
+				if(m_SpeedMultiplier<=0){m_SpeedMultiplier=0.0005f;}
 		}
 		else
 		{
 			iForward = (GetKeyPressed('W') ? 1 : 0) - (GetKeyPressed('S') ? 1 : 0); //Key Inputs
 			iHorizontal = (GetKeyPressed('D') ? 1 : 0) - (GetKeyPressed('A') ? 1 : 0); //(Key A - Key B) will give you a 1/0/-1 input axis
 			iUpwards = (GetKeyPressed(VK_SPACE) ? 1 : 0) - (GetKeyPressed(VK_CONTROL) ? 1 : 0);
-			m_SpeedMultiplier = m_SpeedMultiplier + 0.00001f*((GetKeyPressed('Q') ? 1 : 0) - (GetKeyPressed('E') ? 1 : 0));	//Modify how fast the player moves
+			m_SpeedMultiplier = m_SpeedMultiplier + 0.00005f*((GetKeyPressed('Q') ? 1 : 0) - (GetKeyPressed('E') ? 1 : 0));	//Modify how fast the player moves
 				if(m_SpeedMultiplier<=0){m_SpeedMultiplier=0.0001f;}
 		}
 
@@ -71,9 +74,9 @@ void FPControllerComponent::update()
 			if(iUpwards==0.0f & m_SmoothZ<=m_Accel & m_SmoothZ>=-m_Accel){m_SmoothZ=0.0f;}
 		
 		m_Translate = XMFLOAT3(m_SmoothX,m_SmoothY,m_SmoothZ);
-			
-		m_Right = -XMVector3Cross(XMLoadFloat3(&m_AimDirection),m_Up); //Get the vector perpendicular to Aim and Up via Cross Product
-		m_VecTranslate = (m_Translate.x * m_SpeedMultiplier * m_VecAimDirection)
+																		//																	| /
+		m_Right = -XMVector3Cross(XMLoadFloat3(&m_AimDirection),m_Up); //Get the vector perpendicular to Aim and Up via Cross Product		|/--->
+		m_VecTranslate = (m_Translate.x * m_SpeedMultiplier * m_VecAimDirection)	//Multiply each input by each direction
 			+(m_Translate.y * m_SpeedMultiplier * m_Right)
 			+(m_Translate.z * m_SpeedMultiplier * m_Up);
 
@@ -81,7 +84,29 @@ void FPControllerComponent::update()
 		XMVECTOR newPos = curPos + m_VecTranslate;
 		
 		//SET TRANSFORM
-		m_pOwnerGameObject->getTransform().setPosition(newPos);
+		m_pOwnerGameObject->getTransform().setPosition(checkBoundaries(newPos));
 		m_pOwnerGameObject->getTransform().setRotation(m_VecAimDirection);
 	}
+}
+
+XMVECTOR FPControllerComponent::checkBoundaries(XMVECTOR position)
+{
+	//Quick'n'dirty boundary checking	
+	//X
+	if(position.x>m_BoundingCuboid.x/2)
+		position.x = m_BoundingCuboid.x/2;
+	if(position.x<-m_BoundingCuboid.x/2)
+		position.x = -m_BoundingCuboid.x/2;
+	//Y - the floor is 0!
+	if(position.y>m_BoundingCuboid.y)
+		position.y = m_BoundingCuboid.y;
+	if(position.y<0.5f)
+		position.y = 0.5f;
+	//Z
+	if(position.z>m_BoundingCuboid.z/2)
+		position.z = m_BoundingCuboid.z/2;
+	if(position.z<-m_BoundingCuboid.z/2)
+		position.z = -m_BoundingCuboid.z/2;
+
+	return position;
 }
--- GP2BaseCode-Lab-8-Cleaned/GP2BaseCode/GameApplication/FPControllerComponent.h
+++ GP2BaseCode-Lab-8-Cleaned/GP2BaseCode/GameApplication/FPControllerComponent.h
@@ -11,46 +11,49 @@ class FPControllerComponent : public GameComponent
 public:
 	FPControllerComponent()
 	{
-		m_SpeedMultiplier = 0.005;
-		m_Accel = 0.01f;
-		m_MouseSensitivity = 0.2f;
-		m_ControllerAimSensitivity = 0.05f;
-		m_ControllerInput=false;
-
-		m_Translate = XMFLOAT3(0.0f,0.0f,0.0f);
-		m_AimAngle = XMFLOAT3(0.0f,0.0f,0.0f);
-		m_AimDirection = XMFLOAT3(0.0f,0.0f,0.0f);
-		XMFLOAT3 up = XMFLOAT3(0.0f,1.0f,0.0f);
-		m_Up = XMLoadFloat3(&up);
-		m_SmoothX = 0.0f;
+		m_SpeedMultiplier = 0.01;				
+		m_Accel = 0.01f;						
+		m_MouseSensitivity = 0.2f;				
+		m_ControllerAimSensitivity = 0.1f;		
+		m_ControllerInput=false;				
+			
+		m_Translate = XMFLOAT3(0.0f,0.0f,0.0f);		
+		m_AimAngle = XMFLOAT3(0.0f,0.0f,0.0f);		
+		m_AimDirection = XMFLOAT3(0.0f,0.0f,0.0f);	
+		XMFLOAT3 up = XMFLOAT3(0.0f,1.0f,0.0f);		
+		m_Up = XMLoadFloat3(&up);					
+		m_SmoothX = 0.0f;							
 		m_SmoothY = 0.0f; 
 		m_SmoothZ = 0.0f;
+		m_BoundingCuboid = XMFLOAT3(10.0f, 9.0f, 140.0f);	
 	};
 	~FPControllerComponent(){};
 	void update(); //from parent
+	XMVECTOR checkBoundaries(XMVECTOR position);	//Checking rectangular boundary
 
 private:
-	bool m_ControllerInput;
+	bool m_ControllerInput;					//Toggling between Mouse+Keyboard and Controller inputs
 
-	float m_SpeedMultiplier;
-	float m_MouseSensitivity;
-	float m_ControllerAimSensitivity;
-	float m_Accel;
-	float m_SmoothX;
-	float m_SmoothY;
+	float m_SpeedMultiplier;				//How fast to move the camera around
+	float m_MouseSensitivity;				//How fast the mouse rotates the camera
+	float m_ControllerAimSensitivity;		//How fast the right analogue stick rotates the camera
+	float m_Accel;							//How quickly to reach maximum speed
+	float m_SmoothX;						
+	float m_SmoothY;						//Smoothing out movement
 	float m_SmoothZ;
 
-	float iForward;
+	float iForward;							//Input variables for each direction, ranging from -1 to 1
 	float iHorizontal;
 	float iUpwards;
 
-	XMFLOAT3 m_Translate;
-	XMFLOAT3 m_AimAngle;
+	XMFLOAT3 m_Translate;					//What amount to move the camera by
+	XMFLOAT3 m_AimAngle;					//What angle to aim at, in degrees
+	XMFLOAT3 m_BoundingCuboid;				//Boundaries the camera cannot move out of
 	
-	XMFLOAT3 m_AimDirection;
-	XMVECTOR m_Right;
-	XMVECTOR m_Up;
+	XMFLOAT3 m_AimDirection;				//The angle to aim at converted to a normalized vector
+	XMVECTOR m_Right;						//Vector perpendicular to both the aim direction and up direction
+	XMVECTOR m_Up;							//Unit vector pointing straight upwards
 
-	XMVECTOR m_VecAimDirection;
-	XMVECTOR m_VecTranslate;
+	XMVECTOR m_VecAimDirection;				//XMVECTORs for aim and movement, for addition/multiplication purposes
+	XMVECTOR m_VecTranslate;				
 };
