14f7beabe61ed555157fa022daa2406372590d8f
AlexTrevett <atrevett94@gmail.com>
2013-12-20 16:59:47 +0000

    Updated Scene

--- GP2BaseCode-Lab-8-Cleaned/GP2BaseCode/Lab1/MyGame.cpp
+++ GP2BaseCode-Lab-8-Cleaned/GP2BaseCode/Lab1/MyGame.cpp
@@ -121,7 +121,8 @@ bool MyGame::initGame()
 		iter->second->addComponent(pMaterial);
 		VisualComponent *pVisual=static_cast<VisualComponent*>(iter->second->getComponent("Visual"));
 		pVisual->createVertexLayout(m_pRenderer);
-		iter->second->getTransform().setPosition(0.0f, -1.1f, 0.0f);
+		iter->second->getTransform().setScale(2.0f,2.0f,2.0f);
+		iter->second->getTransform().setPosition(0.0f, -1.5f, 40.0f);
 	}
 	
 	m_GameObjectList.push_back(pParaCar);
@@ -131,21 +132,38 @@ bool MyGame::initGame()
 	for(GameObject::ChildrenGameObjectsIter iter=pCorridor->getFirstChild();iter!=pCorridor->getLastChild();iter++)
 	{
 		pMaterial=new Material();
-		pMaterial->loadEffect("Effects/DirectionalLight.fx",m_pRenderer);
-		//pMaterial->loadDiffuseTexture("Textures/planet_earth.bmp",m_pRenderer);
+		pMaterial->loadEffect("Effects/Parallax.fx",m_pRenderer);
+		pMaterial->loadDecalView("Textures/MetalBase.jpg", m_pRenderer);
+		pMaterial->loadNormalMap("Textures/MetalBase_Normal.pg", m_pRenderer);
 		iter->second->addComponent(pMaterial);
 		VisualComponent *pVisual=static_cast<VisualComponent*>(iter->second->getComponent("Visual"));
 		pVisual->createVertexLayout(m_pRenderer);
 		iter->second->getTransform().setScale(0.6f, 0.6f, 0.6f);
 		iter->second->getTransform().setPosition(0.0f, 0.0f, 0.0f);
-		iter->second->getTransform().setRotation(-3.9f, 0.0f, 0.0f);
+		iter->second->getTransform().setRotation(-3.925f, 0.0f, 0.0f);
 	}
 
 	m_GameObjectList.push_back(pCorridor);
-	
-	// planet
-	GameObject *pEarth=m_ModelLoader.loadModelFromFile("Models/planet_earth.fbx",m_pRenderer);
-	for(GameObject::ChildrenGameObjectsIter iter=pEarth->getFirstChild();iter!=pEarth->getLastChild();iter++)
+
+
+	GameObject *pPipe=m_ModelLoader.loadModelFromFile("Models/Pipe.fbx",m_pRenderer);
+	for(GameObject::ChildrenGameObjectsIter iter=pPipe->getFirstChild();iter!=pPipe->getLastChild();iter++)
+	{
+		pMaterial=new Material();
+		pMaterial->loadEffect("Effects/DirectionalLight.fx",m_pRenderer);
+		pMaterial->loadDiffuseTexture("Textures/MetalBase.jpg",m_pRenderer);
+		iter->second->addComponent(pMaterial);
+		VisualComponent *pVisual=static_cast<VisualComponent*>(iter->second->getComponent("Visual"));
+		pVisual->createVertexLayout(m_pRenderer);
+		iter->second->getTransform().setScale(0.315f, 0.315f, 0.315f);
+		iter->second->getTransform().setPosition(-6.25f, 0.75f, 38.0f);
+		iter->second->getTransform().setRotation(0.0f, 0.0f, 1.25f);
+	}
+
+	m_GameObjectList.push_back(pPipe);
+
+	GameObject *pLights=m_ModelLoader.loadModelFromFile("Models/Lights.fbx",m_pRenderer);
+	for(GameObject::ChildrenGameObjectsIter iter=pLights->getFirstChild();iter!=pLights->getLastChild();iter++)
 	{
 		pMaterial=new Material();
 		pMaterial->loadEffect("Effects/DirectionalLight.fx",m_pRenderer);
@@ -153,11 +171,49 @@ bool MyGame::initGame()
 		iter->second->addComponent(pMaterial);
 		VisualComponent *pVisual=static_cast<VisualComponent*>(iter->second->getComponent("Visual"));
 		pVisual->createVertexLayout(m_pRenderer);
+		iter->second->getTransform().setScale(0.5f, 0.5f, 0.5f);
+		iter->second->getTransform().setPosition(0.0f, 14.5f, 0.0f);
+		iter->second->getTransform().setRotation(-2.355f, 0.0f, 0.0f);
+	}
+
+	m_GameObjectList.push_back(pLights);
+
+GameObject *pDoor=m_ModelLoader.loadModelFromFile("Models/Door.fbx",m_pRenderer);
+	for(GameObject::ChildrenGameObjectsIter iter=pDoor->getFirstChild();iter!=pDoor->getLastChild();iter++)
+	{
+		pMaterial=new Material();
+		pMaterial->loadEffect("Effects/Parallax.fx",m_pRenderer);
+		pMaterial->loadDecalView("Textures/ma_door_01.jpg",m_pRenderer);
+		iter->second->addComponent(pMaterial);
+		VisualComponent *pVisual=static_cast<VisualComponent*>(iter->second->getComponent("Visual"));
+		pVisual->createVertexLayout(m_pRenderer);
 		iter->second->getTransform().setScale(0.4f, 0.4f, 0.4f);
-		iter->second->getTransform().setPosition(0.0f, 5.0f, -85.0f);
+		iter->second->getTransform().setPosition(0.0f, 5.8f, -55.0f);
+		iter->second->getTransform().setRotation(-0.8f, 0.0f, 0.0f);
+	}
+
+	m_GameObjectList.push_back(pDoor);
+	
+	
+	GameObject *pEarth=m_ModelLoader.loadModelFromFile("Models/planet_earth.fbx",m_pRenderer);
+	for(GameObject::ChildrenGameObjectsIter iter=pEarth->getFirstChild();iter!=pEarth->getLastChild();iter++)
+	{
+		pMaterial=new Material();
+		pMaterial->loadEffect("Effects/Parallax.fx",m_pRenderer);
+		pMaterial->loadDecalView("Textures/mars_color.jpg", m_pRenderer);
+		pMaterial->loadNormalMap("Textures/mars_normal.jpg", m_pRenderer);
+		//pMaterial->loadHeightMap("Textures/mars_Height.jpg", m_pRenderer);
+		iter->second->addComponent(pMaterial);
+		VisualComponent *pVisual=static_cast<VisualComponent*>(iter->second->getComponent("Visual"));
+		pVisual->createVertexLayout(m_pRenderer);
+		iter->second->getTransform().setScale(1.6f, 1.6f, 1.6f);
+		iter->second->getTransform().setPosition(0.0f, 5.0f, -120.0f);
+		iter->second->getTransform().setRotation(0.0f,1.0f,4.0f);
 	}
 	
 	m_GameObjectList.push_back(pEarth);
+	
+	
 
 	//Skydome
 	GameObject *pSpace=m_ModelLoader.loadModelFromFile("Models/skSphere.fbx",m_pRenderer);
@@ -175,7 +231,7 @@ bool MyGame::initGame()
 	}
 	m_GameObjectList.push_back(pSpace);
 	
-	//Spaceship
+	
 	OrbitalMovementComponent *pOrbMoveSpaceShip = new OrbitalMovementComponent(20.0f, 0.0f, 0.0f, 0.0f, "z", 10, 0.0f, 5.0, 0.0f);
 	GameObject *pSpaceShip=m_ModelLoader.loadModelFromFile("Models/spaceship01.fbx",m_pRenderer);
 	for(GameObject::ChildrenGameObjectsIter iter=pSpaceShip->getFirstChild();iter!=pSpaceShip->getLastChild();iter++)
@@ -192,8 +248,9 @@ bool MyGame::initGame()
 		//iter->second->getTransform().setPosition(0.0f, 0.0f, 0.0f);
 	}
 	m_GameObjectList.push_back(pSpaceShip);
-
-
+	
+	
+	
 
 
 	return true;
